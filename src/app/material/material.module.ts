b81de0e0ef16309ce6fe6e4240eca145c46164fd
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-17 15:35:17 -0300

    Adiciona botão para testar o angular material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material";
+
+const MaterialComponets = [MatButtonModule];
+
+@NgModule({
+  imports: [MaterialComponets],
+  exports: [MaterialComponets],
+})
+export class MaterialModule {}
