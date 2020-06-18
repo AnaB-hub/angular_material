77bd286e29e196b0628758c97e57fad060ee3191
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-18 08:03:15 -0300

    Trabalhando com ícones

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule, MatButtonToggleModule } from "@angular/material";
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+} from "@angular/material";
 
-const MaterialComponets = [MatButtonModule, MatButtonToggleModule];
+const MaterialComponets = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+];
 
 @NgModule({
   imports: [MaterialComponets],
