7e925beb2bfd38113c10ed4d65316af1733cf4f2
ana.garcia <ana.garcia@castinformatica.onmicrosoft.com>
2020-06-18 14:07:28 -0300

    Trabalhando com Tooltip

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -23,6 +23,7 @@ import {
   MatRadioModule,
   MatDatepickerModule,
   MatNativeDateModule,
+  MatTooltipModule,
 } from "@angular/material";
 
 const material = [
@@ -49,6 +50,7 @@ const material = [
   MatRadioModule,
   MatDatepickerModule,
   MatNativeDateModule,
+  MatTooltipModule,
 ];
 
 @NgModule({
